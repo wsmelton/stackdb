c0c2b485a760bdbdade713aa6460c146e94ea683
Shawn Melton <wshawnmelton@gmail.com>
2017-02-16 18:43:45 -0600

    Created one command to build database and tables

--- functions/New-SEDbTables.ps1
+++
@@ -1,6 +0,0 @@
-function New-SETables
-{
-	[cmdletbinding()]
-	param()
-    $sqlCn = New-Object System.Data.SqlClient.SqlConnection("Data Source=$($server);Integrated Security=SSPI;Initial Catalog=$($database)");
-}
