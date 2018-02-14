01d9d004daec05842e22a9d7a4b150d614e3fc1b
Shawn Melton <wshawnmelton@gmail.com>
2018-02-13 22:47:50 -0600

    update message, add dbatools commands

--- functions/Import-StackArchive.ps1
+++ functions/Import-StackArchive.ps1
@@ -1,158 +1,176 @@
 function Import-StackArchive {
-<#
-	.SYNOPSIS
-		Pulls XML dump file and bulk loads into database
-	.DESCRIPTION
-		Imports the XML dump file and then uses bulk load method to import into specified database
-	.PARAMETER filepath
-		The path to where the uncompressed XML files are located for the StackExchange site
-    .PARAMETER sqlserver
-        SQL Server instance were database is located
-    .PARAMETER database
-        Database where you want to import the data
-    .PARAMETER schema
-        Defaults to "dbo", specify schema if using specific one
-    .PARAMETER tableList
-        If you want to granularly load tables for demos or refresh a table.
-    .PARAMETER batchSize
-        Set the batch size for the bulk copy process, default to 2000. May want to adjust when loading SO database.
-	.EXAMPLE
-	Import one table to specific schema
-    Import-StackArchive -folder 'C:\temp\quant.stackexchange.com' -server MANATARMS\SQL12 -database StackExchange -schema quant -tableList Badges
-	.EXAMPLE
-	Import multiple tables to specific schema
-	Import-StackArchive -folder 'C:\temp\quant.stackexchange.com' -server MANATARMS\SQL12 -database StackExchange -schema quant -tableList 'Badges','Votes'
-	.EXAMPLE
-	Import all files into database, using default schema, and verbose logging
-	$files = Get-ChildItem 'C:\temp\quant.stackexchange.com' -filter *.xml | Select-Object -ExpandProperty BaseName
-    Import-StackArchive -folder 'C:\temp\quant.stackexchange.com' -server MANATARMS\SQL12 -database StackExchange -tableList $files -Verbose
-#>
+    <#
+        .SYNOPSIS
+            Pulls XML dump file and bulk loads into database
 
+        .DESCRIPTION
+            Imports the XML dump file and then uses bulk load method to import into specified database
+
+        .PARAMETER Folder
+            The path to where the uncompressed XML files are located for the StackExchange site
+
+        .PARAMETER SqlServer
+            SQL Server instance were database is located
+
+        .PARAMETER SqlCredential
+            Credential object to connect as another user to SQL Server.
+
+        .PARAMETER Database
+            Database where you want to import the data
+
+        .PARAMETER Schema
+            Defaults to "dbo", specify schema if using specific one
+
+        .PARAMETER TableList
+            If you want to granularly load tables for demos or refresh a table.
+
+        .PARAMETER BatchSize
+            Set the batch size for the bulk copy process, default to 2000. May want to adjust when loading SO database.
+
+        .EXAMPLE
+            Import-StackArchive -Folder 'C:\quant.stackexchange.com' -SqlServer SQL12 -Database StackExchange -Schema quant -TableList Badges
+
+            Import the Badges table from quant archive into the database StackExchange on SQL12
+
+        .EXAMPLE
+            Import-StackArchive -Folder 'C:\quant.stackexchange.com' -SqlServer SQL12 -Database StackExchange -Schema quant -TableList 'Badges','Votes'
+
+            Import the Badges and Votes data from quant archive into the database StachExchange on SQL12
+
+        .EXAMPLE
+            Import-StackArchive -Folder 'C:\quant.stackexchange.com' -SqlServer SQL12 -Database StackExchange
+
+            Import all data from quant archive into the database StackExchange on SQL12.
+    #>
     [cmdletbinding()]
     param (
-        [string]$folder,
-        [string]$sqlserver,
-        [string]$database,
-        [string]$schema = 'dbo',
-        [string[]]$tableList,
-        [int]$batchSize
+        [string]$Folder,
+        [string]$SqlServer,
+        [PSCredential]$SqlCredential,
+        [string]$Database,
+        [string]$Schema = 'dbo',
+        [ValidateSet('Badges', 'Comments', 'PostHistory', 'PostLinks', 'Posts', 'Tags', 'Users', 'Votes')]
+        [string[]]$TableList,
+        [int]$BatchSize
     )
 
-    if ( !(Test-Path $folder) ) {
-        throw "The path provided does not exist!!"
+    if ( !(Test-Path $Folder) ) {
+        Stop-PSFFunction -Message "$Folder does not exist!"
+        return
     }
-    if ($batchSize -eq 0) {
-        $batchSize = 2000
+    if (Test-PSFParameterBinding 'BatchSize' -Not) {
+        $BatchSize = 2000
     }
-    if ($tableList.Length -eq 0) {
-        $tableList = 'Badges','Comments','PostHistory','PostLinks','Posts','Tags','Users','Votes'
+    if (Test-PSFParameterBinding 'TableList' -Not) {
+        $TableList = 'Badges', 'Comments', 'PostHistory', 'PostLinks', 'Posts', 'Tags', 'Users', 'Votes'
     }
 
-#    Push-Location
-#    Set-Location $folder
+    $fileList = 'Badges.xml', 'Comments.xml', 'PostHistory.xml', 'PostLinks.xml', 'Posts.xml', 'Tags.xml', 'Users.xml', 'Votes.xml'
 
-    $fileList = 'Badges.xml','Comments.xml','PostHistory.xml','PostLinks.xml','Posts.xml','Tags.xml','Users.xml','Votes.xml'
+    $files = Get-ChildItem -Path $Folder | Where-Object Name -in $FileList | Select-Object Name, BaseName
 
-    $files = Get-ChildItem -Path $folder | Where-Object Name -in $fileList | Select-Object Name, BaseName
-
-    $sqlcn = New-SqlCn -sqlserver $sqlserver -database $database
+    Write-PSFMessage -Level Verbose -Message "Connecting to $SqlServer"
+    try {
+        $instance = Connect-DbaInstance -SqlInstance $SqlServer -SqlCredential $SqlCredential -ClientName "StackDb PowerShell Module - StackExchange Archive"
+    }
+    catch {
+        Stop-PSFFunction -Message "Failure" -Category ConnectionError -Target $SqlServer -ErrorRecord $_
+        return
+    }
 
     try {
-        $bulkLoad = New-Object ("System.Data.SqlClient.SqlBulkCopy") $sqlcn
+        $bulkLoad = New-Object ("System.Data.SqlClient.SqlBulkCopy") $instance.ConnectionContext.ConnectionString
         $bulkLoad.BatchSize = $batchSize
     }
     catch {
-        throw "Error`: $_"
+        Stop-PSFFunction -Message "Issue creating bulk load object" -ErrorRecord $_
+        return
     }
 
-    $totalfiles = $files.Count
-    $totalTables = $tableList.Count
     $i=$x = 1
     foreach ($f in $files) {
         Write-Progress -Id 1 -Activity "Working on files" -Status "Processing $($f.Name)" -PercentComplete $i
-        foreach ($t in $tableList) {
+        foreach ($t in $TableList) {
             Write-Progress -Id 2 -Activity "Working on Tables" -Status "Processing $t" -PercentComplete $x
             if (Test-Path $f.Name ) {
                 if ($t -eq $f.BaseName) {
                     switch ($t) {
                         "Badges" {
-                            Write-Verbose "Found Badges file..."
+                            Write-PSFMessage -Level Verbose -Message "Found Badges file..."
                             [xml]$badges = Get-Content $f.Name
-                            $badgesDt = $badges.badges.Row | Select-Object UserId, Name, Date | Out-DataTable
+                            $badgesDt = $badges.badges.Row | Select-Object UserId, Name, Date | Out-DbaDataTable
                             $bulkLoad.DestinationTableName = "$schema.$t"
-                            Write-Verbose "Bulk loading Badges file..."
+                            Write-PSFMessage -Level Verbose -Message "Bulk loading Badges file..."
                             $bulkLoad.WriteToServer($badgesDt)
                         }
                         "Comments" {
-                            Write-Verbose "Found Comments file..."
+                            Write-PSFMessage -Level Verbose -Message "Found Comments file..."
                             [xml]$comments = Get-Content $f.Name
-                            $commentsDt = $comments.comments.row | Select-Object Id, PostId, Score, Text, CreationDate, UserId | Out-DataTable
+                            $commentsDt = $comments.comments.row | Select-Object Id, PostId, Score, Text, CreationDate, UserId | Out-DbaDataTable
                             $bulkLoad.DestinationTableName = "$schema.$t"
-                            Write-Verbose "Bulk loading Comments file..."
+                            Write-PSFMessage -Level Verbose -Message "Bulk loading Comments file..."
                             $bulkLoad.WriteToServer($commentsDt)
                         }
                         "PostHistory" {
-                            Write-Verbose "Found PostHistory file..."
+                            Write-PSFMessage -Level Verbose -Message "Found PostHistory file..."
                             [xml]$postHistory = Get-Content $f.Name
                             $postHistoryDt = $postHistory.posthistory.row | Select-Object Id, PostHistoryTypeId, PostId, RevisionGUID, CreationDate,
-                            UserId, UserDisplayName, Comment, Text, CloseReasonId | Out-DataTable
+                            UserId, UserDisplayName, Comment, Text, CloseReasonId | Out-DbaDataTable
                             $bulkLoad.DestinationTableName = "$schema.$t"
-                            Write-Verbose "Bulk loading PostHistory file..."
+                            Write-PSFMessage -Level Verbose -Message "Bulk loading PostHistory file..."
                             $bulkLoad.WriteToServer($postHistoryDt)
                         }
                         "PostLinks" {
-                            Write-Verbose "Found PostLinks file..."
+                            Write-PSFMessage -Level Verbose -Message "Found PostLinks file..."
                             [xml]$postLink = Get-Content $f.Name
-                            $postLinkDt = $postLink.postlinks.row | Select-Object Id, CreationDate, PostId, RelatedPostId, LinkTypeId | Out-DataTable
+                            $postLinkDt = $postLink.postlinks.row | Select-Object Id, CreationDate, PostId, RelatedPostId, LinkTypeId | Out-DbaDataTable
                             $bulkLoad.DestinationTableName = "$schema.$t"
-                            Write-Verbose "Bulk loading PostLinks file..."
+                            Write-PSFMessage -Level Verbose -Message "Bulk loading PostLinks file..."
                             $bulkLoad.WriteToServer($postLinkDt)
                         }
                         "Posts" {
-                            Write-Verbose "Found Posts file..."
+                            Write-PSFMessage -Level Verbose -Message "Found Posts file..."
                             [xml]$posts = Get-Content $f.Name
                             $postsDt = $posts.posts.row | Select-Object Id, PostTypeId, ParentId, AcceptedAnswerId, CreationDate, Score, ViewCount,
                             Body, OwnerUserId, LastEditorUserId, LastEditorDisplayName, LastEditDate, LastActivityDate, CommunityOwnedDate,
-                            ClosedDate, Title, Tags, AnswerCount, CommentCount, FavoriteCount | Out-DataTable
+                            ClosedDate, Title, Tags, AnswerCount, CommentCount, FavoriteCount | Out-DbaDataTable
                             $bulkLoad.DestinationTableName = "$schema.$t"
-                            Write-Verbose "Bulk loading Posts file..."
+                            Write-PSFMessage -Level Verbose -Message "Bulk loading Posts file..."
                             $bulkLoad.WriteToServer($postsDt)
                         }
                         "Tags" {
-                            Write-Verbose "Found Tags file..."
+                            Write-PSFMessage -Level Verbose -Message "Found Tags file..."
                             [xml]$tags = Get-Content $f.Name
-                            $tagsDt = $tags.tags.row | Select-Object Id, TagName, Count, ExcerptPostId, WikiPostId | Out-DataTable
+                            $tagsDt = $tags.tags.row | Select-Object Id, TagName, Count, ExcerptPostId, WikiPostId | Out-DbaDataTable
                             $bulkLoad.DestinationTableName = "$schema.$t"
-                            Write-Verbose "Bulk loading Tags file..."
+                            Write-PSFMessage -Level Verbose -Message "Bulk loading Tags file..."
                             $bulkLoad.WriteToServer($tagsDt)
                         }
                         "Users" {
-                            Write-Verbose "Found Users file..."
+                            Write-PSFMessage -Level Verbose -Message "Found Users file..."
                             [xml]$users = Get-Content $f.Name
                             $usersDt = $users.users.row | Select-Object Id, Reputation, CreationDate, DisplayName, EmailHash, LastAccessDate, WebsiteUrl,
-                            Location, Age, AboutMe, Views, UpVotes, DownVotes | Out-DataTable
+                            Location, Age, AboutMe, Views, UpVotes, DownVotes | Out-DbaDataTable
                             $bulkLoad.DestinationTableName = "$schema.$t"
-                            Write-Verbose "Bulk loading Users file..."
+                            Write-PSFMessage -Level Verbose -Message "Bulk loading Users file..."
                             $bulkLoad.WriteToServer($usersDt)
                         }
                         "Votes" {
-                            Write-Verbose "Found Votes file..."
+                            Write-PSFMessage -Level Verbose -Message "Found Votes file..."
                             [xml]$votes = Get-Content $f.Name
-                            $votesDt = $votes.votes.row | Select-Object Id, PostId, VoteTypeId, CreationDate, UserId, BountyAmount | Out-DataTable
+                            $votesDt = $votes.votes.row | Select-Object Id, PostId, VoteTypeId, CreationDate, UserId, BountyAmount | Out-DbaDataTable
                             $bulkLoad.DestinationTableName = "$schema.$t"
-                            Write-Verbose "Bulk loading Votes file..."
+                            Write-PSFMessage -Level Verbose -Message "Bulk loading Votes file..."
                             $bulkLoad.WriteToServer($votesDt)
                         }
                     } #end switch $t
                 } #end if test basename
             } #end if test name
             else {
-                Write-Warning "No valid files found in provided directory"
+                Write-PSFMessage -Level Warning -Message "No valid files found in provided directory"
             }
             $x++
         } #end foreach tablelist
         $i++
     } #end foreach file
-    $sqlcn.Close()
-#    Pop-Location
 }
