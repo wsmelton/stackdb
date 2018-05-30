c268d4c20b5a344d26d673a591a134c9aea5e39f
wsmelton <wshawnmelton@gmail.com>
2018-05-30 10:12:18 -0500

    big rework, had to remove file list part

--- functions/Get-StackArchive.ps1
+++ functions/Get-StackArchive.ps1
@@ -1,13 +1,13 @@
 function Get-StackArchive {
-	<#
+    <#
 	.SYNOPSIS
 		Downloads the specified 7-Zip file for the site specified
 	.DESCRIPTION
 		Downloads the Archive of the specified StackExchange network site
 	.PARAMETER SiteName
 		String. The [siteName].stackexchange.com; with meta sites you can reference them as "meta.dba".
-	.PARAMETER ListAvailable
-		Switch to just list the sites found available to download, filters when siteName provided.
+	.PARAMETER ListSite
+		Output the sites.xml content with details on each site data archive.
     .PARAMETER DownloadPath
         String. The path to download the archive file to on your local or network directory
 	.PARAMETER IncludeMeta
@@ -21,79 +21,86 @@ function Get-StackArchive {
 		Get-StackArchive -SiteName skeptics.meta -DownloadPath 'C:\temp\MyDumpSite'
 		Download skeptics meta data dump in StackExchange network
 	.EXAMPLE
-		Get-StackArchive -SiteName woodworking -ListAvailable
-		Get list of files for given site that are available, includes date and size
-#>
-	[CmdletBinding(DefaultParameterSetName="Default")]
-	param (
-		[ValidateNotNull()]
-		[string]$SiteName,
-		[ValidateNotNull()]
-		[switch]$ListAvailable,
-		[Parameter(ParameterSetName="Download")]
-		[string]$DownloadPath,
-		[Parameter(ParameterSetName="Download")]
-		[switch]$IncludeMeta,
-		[Parameter(ParameterSetName="Download")]
-		[switch]$Force
-	)
-    [string]$SEArchiveUrl = 'https://archive.org/download/stackexchange'
-	[string]$siteXmlUrl = 'https://archive.org/download/stackexchange/Sites.xml'
-    Write-PSFMessage -Level Verbose -Message "SE Archive URL: $SEArchiveUrl"
-    try {
-        $site = Invoke-WebRequest -Uri $SEArchiveUrl -UseBasicParsing
-        $siteDumpList = ($site.Links | Where-Object innerHtml -match "7z").innerText
-        Write-PSFMessage -Level Verbose -Message "Total number of files found on SE Archive: $($siteDumpList.Count)"
+		Get-StackArchive -SiteName woodworking -ListSite
+		Get site details for woodworking
+    #>
+    [CmdletBinding()]
+    param (
+        [ValidateNotNull()]
+        [string]$SiteName,
+        [ValidateNotNull()]
+        [switch]$ListSite,
+        [string]$DownloadPath,
+        [switch]$IncludeMeta,
+        [switch]$Force
+    )
+    begin {
+        [string]$SEArchiveUrl = 'https://archive.org/download/stackexchange'
+        [string]$siteXmlUrl = 'https://archive.org/download/stackexchange/Sites.xml'
+        Write-PSFMessage -Level Verbose -Message "SE Archive URL: $SEArchiveUrl"
+        Write-PSFMessage -Level Verbose -Message "SE Archive URL: $siteXmlUrl"
     }
-    catch {
-		Write-PSFMessage -Level Error -Message "Issue getting content from $SEArchiveURL" -ErrorRecord $_ -Target "SEArchiveUrl"
-    }
+    process {
+        if ( (Test-PSFParameterBinding 'ListSite') -and (Test-PSFParameterBinding 'DownloadPath' -Not) ) {
+            try {
+				Write-PSFMessage -Level Verbose -Message "Pulling $siteXmlUrl content"
+                [xml]$siteXml = (New-Object System.Net.WebClient).DownloadString($siteXmlUrl)
+                $siteList = $siteXml.Sites.Row | Select-Object TinyName, Name, LongName, DatabaseName, TotalQuestions, TotalAnswers, TotalUsers, TotalComments, TotalTags, LastPost
 
-    if ($ListAvailable -and (-not $DownloadPath)) {
-        [xml]$siteXml = (New-Object System.Net.WebClient).DownloadString($siteXmlUrl)
-		$siteXml.Sites.Row | Select-Object TinyName, Name, LongName, Url, ImageUrl, IconUrl, DatabaseName, TotalQuestions, TotalAnswers, TotalUsers, TotalComments, TotalTags, LastPost
-        <# Attempt to convert file list into usable table output #>
-        $result = $site.AllElements | Where-Object tagName -eq "body" | Select-Object -ExpandProperty innerText
-        $toHash = $result.Split("`r") | ConvertFrom-String | Where-Object P2 -match "7z"
-
-        $siteList = $toHash | Select-Object @{L ="SiteName"; E= {$_.P2}}, @{L="DatePublished"; E= {$_.P3 + $_.P4}},
-        @{L="FileSize"; E= {$_.P5}}
-        if ($SiteName) {
-            $sitelist = $siteList | Where-Object SiteName -match $SiteName
+				Write-PSFMessage -Level Verbose -Message "Sites found: $($siteList.Count)"
+                if ($SiteName) {
+                    $siteList = $siteList | Where-Object TinyName -match $SiteName
+                }
+                return $siteList
+            }
+            catch {
+                Stop-PSFFunction -Message "Issue getting Sites data" -ErrorRecord $_ -Target $siteXmlUrl
+            }
         }
-        return $siteList
+        # provide option to create path if it does not exist
+        elseif ( (Test-PSFParameterBinding 'DownloadPath') ) {
+            if (( !(Test-Path $DownloadPath -PathType Container) ) -and $Force ) {
+                Write-Verbose "Creating Path: $DownloadPath"
+                try {
+                    $result = New-Item $DownloadPath -ItemType Directory -Force
+                    if ($result) {
+                        Write-PSFMessage -Level Output -Message "$($result.FullName) created."
+                    }
+                }
+                catch {
+                    Stop-PSFFunction -Message "Issue creating path" -ErrorRecord $_ -Target $DownloadPath
+                }
+            }
+            if ( Test-Path $DownloadPath ) {
+                try {
+                    $site = Invoke-WebRequest -Uri $SEArchiveUrl
+                    $siteDumpList = ($site.Links | Where-Object innerHtml -match "7z").innerText
+                    Write-PSFMessage -Level Verbose -Message "Total number of files found on SE Archive: $($siteDumpList.Count)"
+                }
+                catch {
+                    Stop-PSFFunction -Message "Issue getting content from $SEArchiveURL" -ErrorRecord $_ -Target $SEArchiveURL
+                }
+
+                $SiteToGrab = $siteDumpList | Where-Object {$_ -match "^$SiteName"}
+                if (Test-PSFParameterBinding 'IncludeMeta' -Not) {
+                    $SiteToGrab = $SiteToGrab | Where-Object {$_ -notmatch "meta"}
+                }
+                foreach ($item in $SiteToGrab) {
+                    try {
+                        $source = "$SEArchiveUrl/$item"
+                        $destination = "$DownloadPath\$($item.Split("/")[-1])"
+                        Write-PSFMessage -Level Verbose -Message "Source path: $source"
+                        Write-PSFMessage -Level Verbose -Message "Destination path: $destination"
+
+                        Write-PSFMessage -Level Output -Message "Downloading $source to $destination"
+                        (New-Object System.Net.WebClient).DownloadFile($source, $destination)
+                        Write-PSFMessage -Level Output -Message "Download completed!"
+                    }
+                    catch {
+                        Stop-PSFFunction -Message "Issue downloading site" -ErrorRecord $_ -Target $item -Continue
+                    }
+                }
+            }
+        }
     }
-
-	# provide option to create path if it does not exist
-	if ( !(Test-Path $DownloadPath -PathType Container) -and $Force ) {
-		Write-Verbose "Creating Path: $DownloadPath"
-		try {
-			$result = New-Item $DownloadPath -ItemType Directory -Force
-			if ($result) {
-				Write-Output "$($result.FullName) created."
-			}
-		}
-		catch {
-			throw "Error`: $_"
-		}
-	}
-
-	$SiteToGrab = $siteDumpList | Where-Object {$_ -match "^$SiteName"}
-	if (-not $IncludeMeta) {
-		$SiteToGrab = $SiteToGrab | Where-Object {$_ -notmatch "meta"}
-	}
-	foreach ($item in $SiteToGrab) {
-		try {
-			$source = "$SEArchiveUrl/$item"
-			$destination = "$DownloadPath\$($item.Split("/")[-1])"
-			Write-Verbose "Source path: $source"
-			Write-Verbose "Destination path: $destination"
-
-			Write-Output "Downloading $source to $destination"
-			(New-Object System.Net.WebClient).DownloadFile($source, $destination)
-		}
-		catch {
-			throw "Error`: $_"
-		}
-	}
 }
